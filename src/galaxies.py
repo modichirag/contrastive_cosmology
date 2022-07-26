79134b293b0754938b054f84ee0a686ef26032f5
modichirag <cmodi@flatironinstitute.org>
2022-07-25 20:02:09 -0400

    velocity assembly bias, new zheng_ab

--- src/galaxies.py
+++ src/galaxies.py
@@ -16,7 +16,10 @@ import os
 import numpy as np 
 # --- nbodykit --- 
 import nbodykit.lab as NBlab
+from astropy.utils.misc import NumpyRNGContext
 from nbodykit.hod import Zheng07Model, HODModel
+from halotools.empirical_models import NFWPhaseSpace, BiasedNFWPhaseSpace
+from halotools.empirical_models.phase_space_models import MonteCarloGalProf
 
 
 def thetahod_literature(paper): 
@@ -99,11 +102,49 @@ def hodGalaxies(halos, p_hod, seed=None, hod_model='zheng07'):
     # populate using HOD
     if hod_model == 'zheng07': 
         hod = halos.populate(Zheng07Model, seed=seed, **p_hod) 
+    elif hod_model == 'zheng07_ab_old': 
+        hod = halos.populate(AssembiasZheng07Model, seed=seed, **p_hod) 
     elif hod_model == 'zheng07_ab': 
         hod = halos.populate(AssembiasZheng07Model, seed=seed, **p_hod) 
+    elif hod_model == 'zheng07_velab': 
+        hod = halos.populate(VelAssembiasZheng07Model, seed=seed, **p_hod) 
     else: 
         raise NotImplementedError
     return hod 
+
+
+
+def hodGalaxies_cache(halos, hod_model='zheng07'): 
+    ''' populate given halo catalog (halos) with galaxies based on HOD model
+1    with p_hod parameters. Currently only supports the Zheng+(2007) model.
+
+    Parameters
+    ----------
+    p_hod : dict
+        dictionary specifying the HOD parameters 
+    '''
+
+    # populate using HOD
+    if hod_model == 'zheng07': 
+        Z07AB = Zheng07Model
+    elif hod_model == 'zheng07_ab': 
+        _Z07AB = AssembiasZheng07Model()
+        Z07AB = _Z07AB.to_halotools(halos.cosmo, 
+                                    halos.attrs['redshift'], 
+                                    halos.attrs['mdef'], 
+                                    sec_haloprop_key='halo_nfw_conc'
+                                )
+
+    elif hod_model == 'zheng07_velab': 
+        _Z07AB = VelAssembiasZheng07Model()
+        Z07AB = _Z07AB.to_halotools(halos.cosmo, 
+                                    halos.attrs['redshift'], 
+                                    halos.attrs['mdef'], 
+                                    sec_haloprop_key='halo_nfw_conc'
+                                )
+    else: 
+        raise NotImplementedError
+    return Z07AB
 
 
 def BOSSGalaxies(sample='lowz-south'): 
@@ -208,3 +249,281 @@ class AssembiasZheng07Model(HODModel):
         model['satellites_occupation'] = satocc
         model['satellites_profile'] = satsprof
         return HodModelFactory(**model)
+
+
+
+
+class VelAssembiasZheng07Model(HODModel):
+    @staticmethod
+    def to_halotools(cosmo, redshift, mdef, concentration_key=None, **kwargs):
+        """
+            Return the Zheng 07 HOD model decorated with assembly bias.
+
+        Parameters
+        ----------
+        cosmo :
+            the nbodykit or astropy Cosmology object to use in the model
+        redshift : float
+            the desired redshift of the model
+        mdef : str, optional
+            string specifying mass definition, used for computing default
+            halo radii and concentration; should be 'vir' or 'XXXc' or
+            'XXXm' where 'XXX' is an int specifying the overdensity
+        concentration_key : str
+            the name of the column that will specify concentration; if not
+            provided, the analytic formula from
+            `Dutton and Maccio 2014 <https://arxiv.org/abs/1402.7073>`_
+            is used.
+        **kwargs :
+            additional keywords passed to the model components; see the
+            Halotools documentation for further details
+        Returns
+        -------
+        :class:`~halotools.empirical_models.HodModelFactory`
+            the halotools object implementing the HOD model
+        """
+        from nbodykit.cosmology import Cosmology
+        from halotools.empirical_models import AssembiasZheng07Sats, AssembiasZheng07Cens
+        from halotools.empirical_models import HodModelFactory 
+
+        kwargs.setdefault('modulate_with_cenocc', True)
+
+        # need astropy Cosmology
+        if isinstance(cosmo, Cosmology):
+            cosmo = cosmo.to_astropy()
+
+        # determine concentration key
+        if concentration_key is None:
+            conc_mass_model = 'dutton_maccio14'
+        else:
+            conc_mass_model = 'direct_from_halo_catalog'
+
+        # determine mass column
+        mass_key = 'halo_m' + mdef
+
+        # occupation functions
+        cenocc = AssembiasZheng07Cens(prim_haloprop_key=mass_key, **kwargs)
+        satocc = AssembiasZheng07Sats(prim_haloprop_key=mass_key, cenocc_model=cenocc, **kwargs)
+        satocc._suppress_repeated_param_warning = True
+
+        # profile functions
+        kwargs.update({'cosmology':cosmo, 'redshift':redshift, 'mdef':mdef})
+        censprof = Centrals_vBiasedNFWPhaseSpace(conc_mass_model=conc_mass_model, **kwargs)
+        satsprof = Satellites_vBiasedNFWPhaseSpace(conc_mass_model=conc_mass_model, 
+                conc_gal_bias_bins=np.linspace(0.1, 2, 96), **kwargs)
+
+        # make the model
+        model = {}
+        model['centrals_occupation'] = cenocc
+        model['centrals_profile'] = censprof
+        model['satellites_occupation'] = satocc
+        model['satellites_profile'] = satsprof
+        return HodModelFactory(**model)
+
+
+class Centrals_vBiasedNFWPhaseSpace(NFWPhaseSpace): 
+    ''' Model for the phase space distribution of galaxies
+    in isotropic Jeans equilibrium in an NFW halo profile,
+    based on Navarro, Frenk and White (1995), where
+    
+    * the concentration of the tracers is permitted to differ from the
+    host halo concentration 
+
+    * the velocity dispersion of the tracers is permitted to differ from
+    the host halo velocity dispersion 
+    '''
+    def __init__(self, **kwargs): 
+        super().__init__(**kwargs)
+        self.param_dict = {'eta_vb.centrals': 0.0} 
+
+    def assign_phase_space(self, table, seed=None):
+        r""" Primary method of the `NFWPhaseSpace` class
+        called during the mock-population sequence.
+
+        Parameters
+        -----------
+        table : object
+            `~astropy.table.Table` storing halo catalog.
+
+            After calling the `assign_phase_space` method,
+            the `x`, `y`, `z`, `vx`, `vy`, and `vz`
+            columns of the input ``table`` will be over-written
+            with their host-centric values.
+
+        seed : int, optional
+            Random number seed used in the Monte Carlo realization.
+            Default is None, which will produce stochastic results.
+
+        """
+        phase_space_keys = ["x", "y", "z"]
+        for key in phase_space_keys:
+            table[key][:] = table["halo_" + key][:]
+
+        if seed is not None:
+            seed += 1
+        MonteCarloGalProf.mc_vel(self, table, seed=seed)
+
+    def mc_radial_velocity(self, scaled_radius, total_mass, *profile_params, **kwargs):
+        r"""
+        Method returns a Monte Carlo realization of radial velocities drawn from Gaussians
+        with a width determined by the solution to the isotropic Jeans equation.
+        Parameters
+        ----------
+        scaled_radius : array_like
+            Halo-centric distance *r* scaled by the halo boundary :math:`R_{\Delta}`, so that
+            :math:`0 <= \tilde{r} \equiv r/R_{\Delta} <= 1`. Can be a scalar or numpy array.
+        total_mass: array_like
+            Length-Ngals numpy array storing the halo mass in :math:`M_{\odot}/h`.
+        *profile_params : Sequence of arrays
+            Sequence of length-Ngals array(s) containing the input profile parameter(s).
+            In the simplest case, this sequence has a single element,
+            e.g. a single array storing values of the NFW concentrations of the Ngals galaxies.
+            More generally, there should be a ``profile_params`` sequence item for
+            every parameter in the profile model, each item a length-Ngals array.
+            The sequence must have the same order as ``self.gal_prof_param_keys``.
+        seed : int, optional
+            Random number seed used in the Monte Carlo realization.
+            Default is None, which will produce stochastic results.
+        Returns
+        -------
+        radial_velocities : array_like
+            Array of radial velocities drawn from Gaussians with a width determined by the
+            solution to the isotropic Jeans equation.
+        """
+        virial_velocities = self.virial_velocity(total_mass)
+        radial_dispersions = virial_velocities * self.param_dict['eta_vb.centrals'] 
+
+        seed = kwargs.get('seed', None)
+        with NumpyRNGContext(seed):
+            radial_velocities = np.random.normal(scale=radial_dispersions)
+
+        return radial_velocities
+
+
+class Satellites_vBiasedNFWPhaseSpace(BiasedNFWPhaseSpace): 
+    ''' Model for the phase space distribution of galaxies
+    in isotropic Jeans equilibrium in an NFW halo profile,
+    based on Navarro, Frenk and White (1995), where
+    
+    * the concentration of the tracers is permitted to differ from the
+    host halo concentration 
+
+    * the velocity dispersion of the tracers is permitted to differ from
+    the host halo velocity dispersion 
+    '''
+    def __init__(self, **kwargs): 
+        super().__init__(**kwargs)
+
+    def _initialize_conc_bias_param_dict(self, **kwargs):
+        r""" Set up the appropriate number of keys in the parameter dictionary
+        and give the keys standardized names.
+        """
+        if 'conc_gal_bias_logM_abscissa' in list(kwargs.keys()):
+            _conc_bias_logM_abscissa = np.atleast_1d(
+                kwargs.get('conc_gal_bias_logM_abscissa')).astype('f4')
+            d = ({'conc_gal_bias_param'+str(i): 1.
+                for i in range(len(_conc_bias_logM_abscissa))})
+            d2 = ({'conc_gal_bias_logM_abscissa_param'+str(i): float(logM)
+                for i, logM in enumerate(_conc_bias_logM_abscissa)})
+            self._num_conc_bias_params = len(_conc_bias_logM_abscissa)
+            for key, value in d2.items():
+                d[key] = value
+            return d
+
+        else:
+            return {'conc_gal_bias.satellites': 1., 'eta_vb.satellites': 1.}
+
+    def assign_phase_space(self, table, seed=None):
+        r""" Primary method of the `NFWPhaseSpace` class
+        called during the mock-population sequence.
+
+        Parameters
+        -----------
+        table : object
+            `~astropy.table.Table` storing halo catalog.
+
+            After calling the `assign_phase_space` method,
+            the `x`, `y`, `z`, `vx`, `vy`, and `vz`
+            columns of the input ``table`` will be over-written
+            with their host-centric values.
+
+        seed : int, optional
+            Random number seed used in the Monte Carlo realization.
+            Default is None, which will produce stochastic results.
+
+        """
+        MonteCarloGalProf.mc_pos(self, table=table, seed=seed)
+        if seed is not None:
+            seed += 1
+        self.mc_vel(table, seed=seed)
+
+    def mc_vel(self, table, seed=None):
+        r""" Method assigns a Monte Carlo realization of the Jeans velocity
+        solution to the halos in the input ``table``.
+
+        Parameters
+        -----------
+        table : Astropy Table
+            `astropy.table.Table` object storing the halo catalog.
+            Calling the `mc_vel` method will over-write the existing values of
+            the ``vx``, ``vy`` and ``vz`` columns.
+
+        seed : int, optional
+            Random number seed used in the Monte Carlo realization.
+            Default is None, which will produce stochastic results.
+
+        """
+        vx, vy, vz = MonteCarloGalProf.mc_vel(self, table, seed=seed, overwrite_table_velocities=False, return_velocities=True)
+        if vx is None: 
+            return 
+        # scale velocity by eta_vs (satellite velocity bias) 
+        vx *= self.param_dict['eta_vb.satellites']
+        vy *= self.param_dict['eta_vb.satellites']
+        vz *= self.param_dict['eta_vb.satellites']
+    
+        # add velcotiy to table.
+        table['vx'][:] += vx
+        table['vy'][:] += vy
+        table['vz'][:] += vz
+
+    def calculate_conc_gal_bias(self, seed=None, **kwargs):
+        r""" Calculate the ratio of the galaxy concentration to the halo concentration,
+        :math:`c_{\rm gal}/c_{\rm halo}`.
+        Parameters
+        ----------
+        prim_haloprop : array, optional
+            Array storing the mass-like variable, e.g., ``halo_mvir``.
+            If ``prim_haloprop`` is not passed,
+            then ``table`` keyword argument must be passed.
+        table : object, optional
+            `~astropy.table.Table` storing the halo catalog.
+            If your NFW model is based on the virial definition,
+            then ``halo_mvir`` must appear in the input table,
+            and likewise for other halo mass definitions.
+            If ``table`` is not passed,
+            then ``prim_haloprop`` keyword argument must be passed.
+        Returns
+        -------
+        conc_gal_bias : array_like
+            Ratio of the galaxy concentration to the halo concentration,
+            :math:`c_{\rm gal}/c_{\rm halo}`.
+        """
+        if 'table' in list(kwargs.keys()):
+            table = kwargs['table']
+            mass = table[self.prim_haloprop_key]
+        elif 'prim_haloprop' in list(kwargs.keys()):
+            mass = np.atleast_1d(kwargs['prim_haloprop']).astype('f4')
+        else:
+            msg = ("\nYou must pass either a ``table`` or ``prim_haloprop`` argument \n"
+                "to the ``assign_conc_gal_bias`` function of the ``BiasedNFWPhaseSpace`` class.\n")
+            raise KeyError(msg)
+
+        result = np.zeros_like(mass) + self.param_dict['conc_gal_bias.satellites']
+
+        if 'table' in list(kwargs.keys()):
+            table['conc_gal_bias'][:] = result
+            halo_conc = table['conc_NFWmodel']
+            gal_conc = self._clipped_galaxy_concentration(halo_conc, result)
+            table['conc_galaxy'][:] = gal_conc
+        else:
+            return result
