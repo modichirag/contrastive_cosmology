f8c38ee63bcbdf4c8ae8ea112c0dabcb02ccf59e
modichirag <cmodi@flatironinstitute.org>
2023-01-31 21:28:31 -0500

    read for fastpm halos

---
+++ scripts/hod_scripts/halo_power.py
@@ -0,0 +1,114 @@
+'''
+Generate galaxy catalog
+'''
+import os, sys 
+import numpy as np 
+import argparse, json
+from pmesh.pm import ParticleMesh, RealField
+from nbodykit.lab import FFTPower, ProjectedFFTPower, ArrayMesh
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+#
+sys.path.append('../../src/')
+import halos as Halos
+import tools, hodtools
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--z', type=float, help='redshift')
+parser.add_argument('--id0', type=int, default=0, help='first quijote seed')
+parser.add_argument('--id1', type=int, default=1, help='last quijote seed')
+parser.add_argument('--seed', type=int, default=0, help='seed between 0-999')
+parser.add_argument('--finder', type=str, default="fof", help='which halo finder')
+parser.add_argument('--suffix', type=str, default="", help='suffix for parent folder with z and nnbar')
+parser.add_argument('--fiducial', type=int, default=0, help='for fiducial simulations')
+parser.add_argument('--simulation', type=str, default='quijote', help='for fiducial simulations')
+args = parser.parse_args()
+
+
+id0, id1 = args.id0, args.id1
+zred = args.z
+if zred is None:
+    print("\nERROR: Need to specify redshift\n")
+    sys.exit()
+else:
+    print(f"\nRun with args: {args}\n")
+
+#
+bs, nc = 1000, 256
+pm = ParticleMesh(Nmesh=[nc, nc, nc], BoxSize=bs, dtype='f8')
+#data_dir = '/mnt/ceph/users/cmodi/contrastive/data/z%02d/halos/'%(zred*10)
+
+if args.fiducial:
+    data_dir = '/mnt/ceph/users/cmodi/Quijote/fiducial/power_spectrum/'
+else:
+    data_dir = '/mnt/ceph/users/cmodi/Quijote/latin_hypercube_HR/power_spectrum/'
+
+subfolder = ""
+if args.finder == "fof": subfolder =  'FoF'
+else: subfolder =  'Rockstar'
+if args.simulation == "fastpm" : subfolder = subfolder + "_fastpm"
+savepath = data_dir + f"/{subfolder}/"
+print(f"Save data in {savepath}")
+os.makedirs(savepath, exist_ok=True)
+print()
+#
+
+def get_power_and_save(i_lhc, halos, pm, numd=None):
+
+    save_dir = savepath + '%04d/'%i_lhc
+    os.makedirs(save_dir, exist_ok=True)
+    if numd is None:
+        num = None
+        suffix = ""
+    else:
+        num = int(1e-4 * bs**3)
+        suffix = f"_n{numd:.1e}"
+    
+    #
+    k, ph = hodtools.get_power(halos, pm, num=num) 
+    np.save(save_dir + f"k", k)
+    np.save(save_dir + f"power{suffix}", np.array([k, ph]).T)
+    
+    k, pmu, pell = hodtools.get_power_rsd(halos, pm, num=num, Nmu=12)
+    np.save(save_dir + f"power_rsd{suffix}", pmu.real)
+    np.save(save_dir + f"power_ell{suffix}", pell.real)
+    
+
+
+for i_lhc in range(id0, id1):
+
+    print('LHC %i' % i_lhc)
+    
+    # read in halo catalog
+    if args.simulation == 'fastpm':
+        if args.fiducial: 
+            print("No fiducial runs for FastPM")
+            raise NotImplementedError
+        else: 
+            halos = Halos.FastPM_LHC_HR(i_lhc, z=zred, finder=args.finder)
+    else:
+        if args.fiducial: 
+            halos = Halos.Quijote_fiducial_HR(i_lhc, z=zred, finder=args.finder)
+        else:
+            halos = Halos.Quijote_LHC_HR(i_lhc, z=zred, finder=args.finder)
+
+    print("Halo read")
+
+    for numd in [None, 1e-4, 3e-4, 5e-4, 1e-3]:
+        get_power_and_save(i_lhc, halos, pm, numd=numd)
+    # save_dir = data_dir + '%04d/'%i_lhc
+    # os.makedirs(save_dir, exist_ok=True)
+    # k, ph = hodtools.get_power(halos, pm)
+    # np.save(save_dir + "powerh", np.array([k, ph]).T)
+    # k, ph = hodtools.get_power(halos, pm, num=int(1e-4 * bs**3))
+    # np.save(save_dir + "powerh_1e-4", np.array([k, ph]).T)
+    # #
+    # k, pmu, pell = hodtools.get_power_rsd(halos, pm, Nmu=12)
+    # np.save(save_dir+"powerh_rsd", pmu)
+    # np.save(save_dir+"powerh_ell", pell)
+    
+    # k, pmu, pell = hodtools.get_power_rsd(halos, pm, num=int(1e-4 * bs**3), Nmu=12)
+    # np.save(save_dir+"powerh_1e-4_rsd", pmu)
+    # np.save(save_dir+"powerh_1e-4_ell", pell)
+    
--- src/halos.py
+++ src/halos.py
@@ -7,7 +7,7 @@ module to inferace with different N-body catalogs incl. Quijote
 import os
 import numpy as np 
 import quijote as Quijote 
-
+import nbodykit.lab as NBlab
 
 
 def Quijote_LHC_HR(i, z=1.0, finder='fof'): 
@@ -43,6 +43,76 @@ def Quijote_LHC_HR(i, z=1.0, finder='fof'):
     # read halo catalog 
     halos = Quijote.Halos(halo_folder, z, Om=Om, Ob=Ob, h=h, ns=ns, s8=s8, Mnu=0., finder=finder)
     return halos
+
+
+
+def FastPM_LHC_HR(i, z=1.0, finder='fof'): 
+    ''' Read halo catalog from the high resolution Quijote LHC. 
+
+
+    Parameters
+    ---------- 
+    i : int 
+        ith realization of the Quijote LHC simulations 
+
+    z : float
+        redshift of the halo catalog. Quijote halo catalogs are available at
+        z = 0, 0.5, 1.
+    
+    finder : str
+             Halo finder, FoF only
+    Return
+    ------
+    cat : nbodykit.lab.HaloCatalog 
+        Quijote HR LHC halo catalog  
+    '''
+    # directory that contains the FastPM LHC HR
+    if finder == 'rockstar':
+        print("Rockstar catalogs for FastPM simulations are not available")
+        raise NotImplementedError
+    
+    # convert z_to_a
+    if z in [1.0, 0.5, 0.0]:
+        a = "%0.4f"%(1/(1+z)) 
+    else:
+        print("FastPM catalogs only have snapshots at z=0, 0.5 and 1")
+        raise NotImplementedError
+
+    #Proceed if checks are satisfied
+    # look up cosmology of the LHC realization which is the same as Quijote
+    Om, Ob, h, ns, s8 = Quijote_LHC_cosmo(i)
+    
+    # define cosmology; caution: we don't match sigma8 here
+    bs = 1000.
+    Mnu = 0.
+    cosmo = NBlab.cosmology.Planck15.clone(
+            h=h, 
+            Omega0_b=Ob, 
+            Omega0_cdm=Om - Ob,
+            m_ncdm=[None, Mnu][Mnu > 0.], 
+            n_s=ns) 
+    Ol = 1. - Om 
+    Hz = 100.0 * np.sqrt(Om * (1. + z)**3 + Ol) # km/s/(Mpc/h)
+
+    if finder == 'fof':
+        cat = NBlab.BigFileCatalog(f'/mnt/ceph/users/cmodi/Quijote/latin_hypercube_HR/FoF_fastpm/{i}/fof_{a}/LL-0.200/')
+        cat['Mass'] = cat["Length"] *float(cat.attrs['M0'] * 1e10)
+        # calculate velocity offset
+        rsd_factor = (1. + z) / Hz
+        cat['VelocityOffset'] = cat['Velocity'].compute()*rsd_factor
+
+    cat = NBlab.HaloCatalog(cat, cosmo=cosmo, redshift=z, mdef='vir') 
+    cat['Position'] = (cat['Position'].compute()%bs )
+    cat.attrs['BoxSize'] = np.array([bs, bs, bs])
+    cat.attrs['rsd_factor'] = rsd_factor
+    cat.attrs['Om'] = Om
+    cat.attrs['Ob'] = Ob
+    cat.attrs['Ol'] = Ol
+    cat.attrs['h'] = h 
+    cat.attrs['ns'] = ns
+    cat.attrs['s8'] = s8
+    cat.attrs['Hz'] = Hz # km/s/(Mpc/h)
+    return cat
 
 
 def Quijote_fiducial_HR(i, z=1.0, finder='FoF'): 
